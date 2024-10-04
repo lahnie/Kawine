a1be25e5f375ca4e74c02e8cb0b6362aa47a92a2
lahnie <bvargasmoran@gmail.com>
2024-10-04 02:39:04 -0300

    redo login act

---
+++ app/src/main/java/com/example/kawine/Login.java
@@ -0,0 +1,62 @@
+package com.example.kawine;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.content.Intent;
+import android.widget.Toast;
+
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Login extends AppCompatActivity {
+
+    private EditText editUser, editPassword;
+    private Button btnLogin, btnRegistrar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+        editUser = findViewById(R.id.editUser);
+        editPassword = findViewById(R.id.editPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegistrar = findViewById(R.id.btnRegistrar);
+
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, Register.class);
+                startActivity(intent);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String username = editUser.getText().toString();
+                String password = editPassword.getText().toString();
+
+                if (TextUtils.isEmpty(username) || TextUtils.isEmpty(password)){
+                    Toast.makeText(Login.this, "Por favor ingrese usuario y contraseña.", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (username.equals("admin") && password.equals("1234")){
+                        Intent intent = new Intent(Login.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        Toast.makeText(Login.this, "Usuario o contraseña incorrectos.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+}
